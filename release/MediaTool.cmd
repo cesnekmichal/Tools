c00c061ead0adcaa8e270f5d08c67be4b1afa4b3
Michal <Michal@Michal-PC>
2025-08-31 16:58:23 +0200

    Release: MediaTool.exe

--- release/MediaTool.cmd
+++ release/MediaTool.cmd
@@ -1,3 +1,19 @@
+@echo off
+CHCP 65001
 CD /D "%~dp0"
 
+echo Spoustim kompilaci EXE...
 "c:\Program Files (x86)\Launch4j\launch4jc.exe" MediaTool.xml
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo Kompilace selhala s chybou %ERRORLEVEL%.
+    echo.
+    pause
+) ELSE (
+    echo.
+    echo Kompilace probehla uspesne.
+    echo.
+)
+
+EXIT /B 0
